83275c47336db7fc0d7d71769cc3368c87fcb6c2
Sagleft <sagleft@inbox.ru>
2020-06-15 21:57:24 +0300

    correct typing added

--- utopia.go
+++ utopia.go
@@ -1,4 +1,4 @@
-package main
+package utopiago
 
 import (
 	//"encoding/json"
@@ -8,6 +8,7 @@ import (
     "io/ioutil"
     "strconv"
     "encoding/json"
+    "gopkg.in/grignaak/tribool.v1"
 )
 
 type Query struct {
@@ -25,6 +26,7 @@ type UtopiaClientInterface interface {
 	apiQuery(methodName string) map[string]interface{}
 	GetProfileStatus() map[string]interface{}
 	GetSystemInfo() map[string]interface{}
+	GetOwnContact() map[string]interface{}
 }
 
 func (c UtopiaClient) apiQuery(methodName string, params map[string]string) map[string]interface{} {
@@ -79,20 +81,38 @@ func (c UtopiaClient) GetSystemInfo() map[string]interface{} {
 	return c.apiQuery("getSystemInfo", nil)
 }
 
-func (c UtopiaClient) SetProfileStatus(status string, mood string) map[string]interface{} {
+func (c UtopiaClient) queryResultToString(methodName string, params map[string]string) string {
+	var response map[string]interface{} = c.apiQuery(methodName, params)
+	var resultstr string = fmt.Sprintf("%v", response["result"])
+	return resultstr
+}
+
+func (c UtopiaClient) queryResultToBool(methodName string, params map[string]string) bool {
+	var resultstr string = c.queryResultToString(methodName, params)
+	var result bool = tribool.FromString(resultstr).WithMaybeAsTrue()
+	return result
+}
+
+func (c UtopiaClient) SetProfileStatus(status string, mood string) bool {
 	queryMap := make(map[string]string)
 	queryMap["status"] = status
 	queryMap["mood"] = mood
-	return c.apiQuery("setProfileStatus", queryMap)
+
+	return c.queryResultToBool("setProfileStatus", queryMap)
 }
 
-func main() {
-	client := UtopiaClient{
-		protocol: "http",
-		token:    "C17BF2E95821A6B545DC9A193CBB750B",
-		host:     "127.0.0.1",
-		port:     22791,
-	}
-
-	fmt.Println(client.GetSystemInfo())
+func (c UtopiaClient) GetOwnContact() map[string]interface{} {
+	return response map[string]interface{} = c.apiQuery("getOwnContact", nil)
 }
+
+//func main() {
+//	client := UtopiaClient{
+//		protocol: "http",
+//		token:    "C17BF2E95821A6B545DC9A193CBB750B",
+//		host:     "127.0.0.1",
+//		port:     22791,
+//	}
+//
+//	fmt.Println(client.GetSystemInfo())
+//	fmt.Println(client.SetProfileStatus("Available", "test mood"))
+//}
